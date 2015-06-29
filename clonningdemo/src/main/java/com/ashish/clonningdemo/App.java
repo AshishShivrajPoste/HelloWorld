2a635fce0be76ddaf77615c5f0b9cc9ec364a381
Ashish Poste <ashish.poste@aricent.com>
2015-06-29 16:54:52 +0530

    Adding new project to Respository

---
+++ clonningdemo/src/main/java/com/ashish/clonningdemo/App.java
@@ -0,0 +1,13 @@
+package com.ashish.clonningdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
