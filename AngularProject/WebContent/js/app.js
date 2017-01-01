4f02e59e892fbebb7bb543737b6f3cb4e97bd392
AshishPoste <ashish_2702@yahoo.com>
2017-01-01 13:17:00 +0530

    commiting changes

--- AngularProject/WebContent/js/app.js
+++ AngularProject/WebContent/js/app.js
@@ -2,127 +2,199 @@
  * 
  */
 
-var sampleApp = angular.module("sampleApp",['F1FeederApp.services','firebase']);
+var sampleApp = angular.module("sampleApp",['firebase','F1FeederApp.services','ngRoute']);
+
+sampleApp.factory('userinfo',[function(){
+	var userInfo;
+	return {
+		getUserInfo : function(){
+			return userInfo;
+		},
+		setUserInfo : function(user){
+			userInfo = user;
+		}
+	};
+}]);
 
 sampleApp.config(['$routeProvider',
                   function($routeProvider) {
 	$routeProvider.
 	when('/home', {
 		templateUrl: 'templates/home.html',
-		controller: 'HomeController'
+		controller: 'HomeController',
+		secure: true
 	}).
 	when('/login', {
 		templateUrl: 'templates/firebaseLogin.html',
 		controller: 'LoginController'
 	}).
+	when('/Signup', {
+		templateUrl: 'templates/signup.html',
+		controller: 'SignUpController'
+	}).
 	when('/ShowOrders', {
 		templateUrl: 'templates/show_orders.html',
-		controller: 'ShowOrdersController'
+		controller: 'ShowOrdersController',
+		secure: true
 	}).when('/AddMessageOrder', {
 		templateUrl: 'templates/add_message.html',
-		controller: 'addMessageController'
+		controller: 'addMessageController',
+		secure: true
 	}).when('/EditMessage',{
 		templateUrl:'templates/edit_message.html',
-		controller:'EditController'
+		controller:'EditController',
+		secure: true
 	});
 }]);
-sampleApp.controller('HomeController', function($scope) {
-
-	$scope.message = 'This is Add new order screen';
-
-});
-/*sampleApp.controller('LoginController', function($scope) {
+sampleApp.controller('HomeController', ['$location','userinfo',function($location,userinfo) {
+	var self  = this;
 	
-	$scope.loginUser=function()
-    {
-        var username=$scope.user.name;
-        var password=$scope.user.password;
-        if(username=="admin" && password=="admin123")
-        {
-            page.setUser($scope.user);
-            $location.path( "/home" );
-        }
-        else
-        {
-            $scope.message="Error";
-            $scope.messagecolor="alert alert-danger";
-        }
-    }
+	self.message = 'This is Add new order screen';
+	self.userInfo = userinfo.getUserInfo();
+	self.logout = function (){
+		firebase.auth().signOut().then(function() {
+			console.log("Logout Sucessful");
+			$location.path( "/#" );
+		}, function(error) {
+			console.log("Logout Error");
+		});
+	};
 
-});*/
-//sampleApp.controller('LoginController', function($scope) {
+}]);
+sampleApp.controller('BaseController', ['$location','userinfo',function($location,userinfo) {
+	var self  = this;
+	
+	self.message = 'This is Add new order screen';
+	self.userInfo = userinfo.getUserInfo();
+	self.logout = function (){
+		firebase.auth().signOut().then(function() {
+			console.log("Logout Sucessful");
+			$location.path( "/#" );
+		}, function(error) {
+			console.log("Logout Error");
+		});
+	};
+
+}]);
+
+//sampleApp.controller('LandingController', ['$location','userinfo',function($location,userinfo) {
+//	var self  = this;
 //	
-//	$scope.toggleSignIn = function() {
-//	    	if (!firebase.auth().currentUser) {
-//	        var provider = new firebase.auth.GoogleAuthProvider();
-//	  
-//	        provider.addScope('https://www.googleapis.com/auth/plus.login');
-//	      
-//	        firebase.auth().signInWithPopup(provider).then(function(result) {
-//	          var token = result.credential.accessToken;
-//	          var user = result.user;
-//	          $location.path( "/home" );
-//	        })
-//	    	} else {
-//	    		firebase.auth().signOut();
-//	    	}
-//	      // [START_EXCLUDE]
-//	     // document.getElementById('quickstart-sign-in').disabled = true;
-//	      // [END_EXCLUDE]
-//	    }
-//	    // [END buttoncallback]
-//	    /**
-//	     * initApp handles setting up UI event listeners and registering Firebase auth listeners:
-//	     *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
-//	     *    out, and that is where we update the UI.
-//	     */
-//	   $scope.initApp=function() {
-//	      // Listening for auth state changes.
-//	      // [START authstatelistener]
-//	      firebase.auth().onAuthStateChanged(function(user) {
-//	        if (user) {
-//	          // User is signed in.
-//	          var displayName = user.displayName;
-//	          var email = user.email;
-//	          var emailVerified = user.emailVerified;
-//	          var photoURL = user.photoURL;
-//	          var isAnonymous = user.isAnonymous;
-//	          var uid = user.uid;
-//	          var providerData = user.providerData;
-//	          // [START_EXCLUDE]
-//	          document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-//	          document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-//	          document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-//	          // [END_EXCLUDE]
-//	        } else {
-//	          // User is signed out.
-//	          // [START_EXCLUDE]
-//	          document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-//	          document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
-//	          document.getElementById('quickstart-account-details').textContent = 'null';
-//	          document.getElementById('quickstart-oauthtoken').textContent = 'null';
-//	          // [END_EXCLUDE]
-//	        }
-//	        // [START_EXCLUDE]
-//	        document.getElementById('quickstart-sign-in').disabled = false;
-//	        // [END_EXCLUDE]
-//	      });
-//	      // [END authstatelistener]
-//	     // document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-//	    }
+//	self.message = 'This is Add new order screen';
+//	self.userInfo = userinfo.getUserInfo();
+//	self.logout = function (){
+//		firebase.auth().signOut().then(function() {
+//			console.log("Logout Sucessful");
+//			$location.path( "/#" );
+//		}, function(error) {
+//			console.log("Logout Error");
+//		});
+//	};
 //
-//});
+//}]);
+
+sampleApp.controller('SignUpController',function($scope){
+	$scope.register = function (){
+		firebase.auth().createUserWithEmailAndPassword($scope.user.registrationEmail, $scope.user.registrationPassword).then(function (user){
+			console.log("User Signed in successfully and Logged In")
+		}, function (error){
+			authCtrl.error = error;
+		});	
+	};
+});
+sampleApp.controller('LoginController', ['$scope','$location', 'userinfo', function($scope,$location,userinfo){
+	//var authCtrl = this;
+	var self = this;
+	self.login = function (){
+		console.log("Current User before :" + firebase.auth().currentUser)
+		firebase.auth().signInWithEmailAndPassword(self.user.loginEmail, self.user.loginPassword).then(function (auth){
+			console.log("Login Successful");
+			console.log(auth);
+			console.log("Current User :" + firebase.auth().currentUser);
+			var userInfo = {
+					email:""
+			};
+			
+			userInfo.email= auth.email; 
+			userinfo.setUserInfo(userInfo);
+			$location.path( "/home" );
+		}, function (error){
+			self.error = error;
+			console.log(error);
+		});
+	};
+
+	self.logout = function (){
+		firebase.auth().signOut().then(function() {
+			console.log("Logout Sucessful");
+			$location.path( "/#" );
+		}, function(error) {
+			console.log("Logout Error");
+		});
+	};
+
+
+
+	self.register = function (){
+		firebase.auth().createUserWithEmailAndPassword(self.user.loginEmail, self.user.loginPassword).then(function (user){
+			console.log("User Signed in successfully and Logged In")
+		}, function (error){
+			self.error = error;
+		});	
+	};
+	self.twittersignIn= function(){
+		var provider = new firebase.auth.TwitterAuthProvider();
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+			console.log("Login Successful with Twitter");
+			var token = result.credential.accessToken;
+			var user = result.user;
+			console.log(user);
+			$location.path( "/home" );
+		},function(error){
+			console.log(error);
+		});
+	};
+	self.facebooksignIn= function(){
+		var provider = new firebase.auth.FacebookAuthProvider();
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+			console.log("Login Successful with facebook");
+			var token = result.credential.accessToken;
+			var user = result.user;
+			console.log(user);
+		},function(error){
+			console.log(error);
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			console.log(errorMessage);
+			// The email of the user's account used.
+			var email = error.email;
+			// The firebase.auth.AuthCredential type that was used.
+			var credential = error.credential;
+		});
+	};
+	self.open = function() {
+		$scope.showModal = true;
+	};
+
+	self.ok = function() {
+		$scope.showModal = false;
+	};
+
+	self.cancel = function() {
+		$scope.showModal = false;
+	};
+}]);
 
 sampleApp.controller('ShowOrdersController', function($scope,ergastAPIservice) {
-	
+
 	$scope.msg = 'This is Show orders screen';
 	$scope.orders = [];	
 	ergastAPIservice.getDrivers().success(function (response) {
 		console.log(response);
-        $scope.orders = response.messages;
-    }).error(function (response) {
+		$scope.orders = response.messages;
+	}).error(function (response) {
 		console.log(response);
-    });
+	});
 });
 
 sampleApp.controller('EditController', function($scope,ergastAPIservice) {
@@ -140,14 +212,14 @@ sampleApp.controller('EditController', function($scope,ergastAPIservice) {
 			console.log("failed");
 			//alert( "failure message: " + JSON.stringify({data: data}));
 		});		  
-    }
+	}
 	$scope.editPerson = function(message){
 		console.log("Edit");
 		console.log(message);
 		var result = ergastAPIservice.editMessage(message);
 		result.success(function(data, status, headers, config) {
-		//	console.log(response);
-		   // $scope.orders = response.messages;
+			//	console.log(response);
+			// $scope.orders = response.messages;
 		});
 		result.error(function(data, status, headers, config) {
 			console.log(response);
@@ -157,10 +229,10 @@ sampleApp.controller('EditController', function($scope,ergastAPIservice) {
 
 
 sampleApp.controller('addMessageController', function($scope,ergastAPIservice) {
-	
+
 	$scope.add = function(message) {
 		//$scope.orders.push({ 'author':$scope.message.author, 'message': $scope.message.mseg });
-		
+
 		//console.log($scope.message.author);
 		//console.log($scope.message.author);
 		var dataObj = {
@@ -180,11 +252,11 @@ sampleApp.controller('addMessageController', function($scope,ergastAPIservice) {
 		//
 		$scope.message.author='';
 		$scope.message.mseg='';
-        
-        
-        
-    }
-	
+
+
+
+	}
+
 	$scope.deleteMessage = function(messageID) {
 		//$scope.orders.push({ 'author':$scope.message.author, 'message': $scope.message.mseg });
 		console.log("Ashish");
@@ -207,5 +279,34 @@ sampleApp.controller('addMessageController', function($scope,ergastAPIservice) {
 		//
 		$scope.message.author='';
 		$scope.message.mseg='';   
-    }
+	}
 });
+
+sampleApp.run(['$rootScope', '$location',
+               function ($rootScope, $location) {
+	
+	//Client-side security. Server-side framework MUST add it's 
+	//own security as well since client-based security is easily hacked
+	$rootScope.$on("$routeChangeStart", function (event, next, current) {
+		if (next && next.$$route && next.$$route.secure) {
+			firebase.auth().onAuthStateChanged(function(user) {
+				if (user) {
+					console.log("user is logged in already");
+					console.log("User Details : "+user);
+					$rootScope.userloggedIn=true;
+					$rootScope.email = user.email;
+				} else {
+					if (next.templateUrl == "templates/login.html") {
+						// already going to the login route, no redirect needed
+						$rootScope.userloggedIn=false;
+					} else {
+						// not going to the login route, we should redirect now
+						$location.path("/login");
+						$rootScope.userloggedIn=false;
+					}
+				}
+			});
+		}
+	});
+
+}]);
